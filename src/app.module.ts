138a9b3619a3403c17b39a9ccc3c04ab7739b4e3
Aditya Prasad <aditya.prasad@kiwiinsurance.com>
2026-09-09 14:42:41 +0530

    adding users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { CatsController } from './cats.controller.js';
 import { AuthModule } from './auth/auth.module.js';
+import { UserModule } from './user/user.module.js';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
