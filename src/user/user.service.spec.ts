b8b28ed49429fc504db91b7f886714797fcc6eec
Aditya Prasad <aditya.prasad@kiwiinsurance.com>
2026-09-17 16:37:11 +0530

    adding tests les goooo user test added for getbio

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service.js';
+import { USER_REPOSITORY, UserRepository } from './user-repository/user-repository.interface.js';
 
 describe('UserService', () => {
   let service: UserService;
+  let mockUserRepository: {findByUsername: ReturnType<typeof vi.fn>; create: ReturnType<typeof vi.fn>};
 
   beforeEach(async () => {
+    mockUserRepository = {findByUsername: vi.fn(), create: vi.fn()}
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {provide: USER_REPOSITORY, useValue: mockUserRepository}
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('gives undefined user if username not found', async () => {
+    mockUserRepository.findByUsername.mockResolvedValue(undefined);
+
+    await expect(service.getBio('username_doesnt_exist')).resolves.toBeUndefined();
   });
+  
 });
