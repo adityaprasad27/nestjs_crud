eeec17fef0bb0771cec48b9ac2f42fb08cdc8c3c
Aditya Prasad <aditya.prasad@kiwiinsurance.com>
2026-09-17 17:52:53 +0530

    added tests for register

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -6,6 +6,7 @@ import { createMockPasswordHasher } from './password-hash/password-hasher.mock.j
 import { UserService } from '../user/user.service.js';
 import { TOKEN_SERVICE } from './token/token-service.interface.js';
 import { PASSWORD_HASHER } from './password-hash/password-hasher.interface.js';
+import { ConflictException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -30,4 +31,49 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('throws conflict exception and never hashes or creates when username already exists', async () => {
+    // arrange 
+    mockUserService.findUser.mockResolvedValue({
+      userId: "1",
+      username: "ap",
+      passwordHash: "some-non-existent-hash",
+      bio: ''
+    });
+
+    // act
+    await expect(service.register("ap", "whatever")).rejects.toThrow(ConflictException);
+
+    // check that unwanted things didnt get called
+    expect(mockPasswordHasher.hash).not.toHaveBeenCalled();
+    expect(mockUserService.createUser).not.toHaveBeenCalled();
+  })
+
+  it('should hash password and create user when username free', async () => {
+    // arrange
+    const username = "test";
+    const userId = "1";
+    const password = "whatever"
+    const passwordHash = "hashed-password";
+    const bio = "1"
+    const token = "some-token"
+    mockUserService.createUser.mockResolvedValue({
+      userId,
+      username,
+      passwordHash,
+      bio
+    }); // does this work? does the repreiting name thing work out
+    mockUserService.findUser.mockResolvedValue(undefined);
+    mockPasswordHasher.hash.mockResolvedValue(passwordHash);
+    mockTokenService.sign.mockResolvedValue(token);
+
+    await expect(service.register(username, password)).resolves.toEqual({access_token: token});
+
+    expect(mockPasswordHasher.hash).toHaveBeenCalledWith(password);
+    expect(mockUserService.createUser).toHaveBeenCalledWith({username, passwordHash, bio: ''});
+    expect(mockUserService.findUser).toHaveBeenCalledWith(username);
+    expect(mockTokenService.sign).toHaveBeenCalledWith({
+      sub: userId, username
+    })
+  })
 });
